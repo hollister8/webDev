32f74e9b40a7b833c90e1a1352781227990d92e3
Jihee <hollister7856@gmail.com>
2022-04-28 21:46:26 +0900

    검색어에 밑줄치기

--- script.js
+++ script.js
@@ -17,18 +17,37 @@ $.get('store.json')
         temp(data.products);
     })
 
+function yellowContent(element, tag, val){
+    if (element.includes(val)){
+        let start = element.indexOf(val);
+        let end = start + val.length; // -1
+        
+        let first = element.slice(0, start);
+        let middle = element.slice(start, end);
+        let last = element.slice(end, element.length);
+
+        // console.log(middle)
+        return `<${tag}>${first}<span style="background : yellow;">${middle}</span>${last}</${tag}>`
+    } else {
+        return `<${tag}>${element}</${tag}>`
+    }
+}
+
 function consoleLog(){
     let val = $('.form-control').val();
 
     $.get('store.json')
         .done((data) => {
+            $('.side-container').remove();
             for (let i = 0; i < data.products.length; i++) {
                 if (data.products[i].title.includes(val) || data.products[i].brand.includes(val)){
-                    $('.side-container').remove();
+                    let title_contents = yellowContent(data.products[i].title, 'h4', val);
+                    let brand_contents = yellowContent(data.products[i].brand, 'h6', val);
+                    
                     let template = `<div class="side-container">
                     <img src="/images/${data.products[i].photo}" alt="" class="goods-img">
-                    <h4><span style="background : yellow;">${val}</span>${data.products[i].title}</h4>
-                    <h6>${data.products[i].brand}</h6>
+                    ${title_contents}
+                    ${brand_contents}
                     <p>가격 : ${data.products[i].price}</p>
                     <button type="button" class="btn btn-dark">담기</button>
                     </div>`;
